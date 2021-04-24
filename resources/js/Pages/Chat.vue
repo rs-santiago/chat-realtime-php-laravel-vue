1af3f75b3f4c020e59dd93befe59397da48c744c
Rodrigo Santiago <rs.santiago1997@gmail.com>
2021-04-24 01:48:10 -0300

    first commit

---
+++ resources/js/Pages/Chat.vue
@@ -0,0 +1,176 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Chat
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px; max-height: 400px; ">
+
+                    <!-- List users -->
+                    <div class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-200 overflow-y-scroll">
+                        <ul>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rodrigo S. Santiago
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Fulano S. A. Coisa
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Box message -->
+                    <div class="w-9/12 flex flex-col justify-between">
+
+                        <!-- message -->
+                        <div class="w-full p-6 flex flex-col overflow-y-scroll">
+                            <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md message-from-me" style="max-width: 75%">
+                                    Olá
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                            <div class="w-full mb-3">
+                                <p class="inline-block p-2 rounded-md message-to-me" style="max-width: 75%">
+                                    Oi
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                                                        <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md message-from-me" style="max-width: 75%">
+                                    Olá
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                            <div class="w-full mb-3">
+                                <p class="inline-block p-2 rounded-md message-to-me" style="max-width: 75%">
+                                    Oi
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                                                        <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md message-from-me" style="max-width: 75%">
+                                    Olá
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                            <div class="w-full mb-3">
+                                <p class="inline-block p-2 rounded-md message-to-me" style="max-width: 75%">
+                                    Oi
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                                                        <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md message-from-me" style="max-width: 75%">
+                                    Olá
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                            <div class="w-full mb-3">
+                                <p class="inline-block p-2 rounded-md message-to-me" style="max-width: 75%">
+                                    Oi
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                                                        <div class="w-full mb-3 text-right">
+                                <p class="inline-block p-2 rounded-md message-from-me" style="max-width: 75%">
+                                    Olá
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                            <div class="w-full mb-3">
+                                <p class="inline-block p-2 rounded-md message-to-me" style="max-width: 75%">
+                                    Oi
+                                </p>
+                                <span class="block text-xs text-gray-500">24/04/2021 00:41</span>
+                            </div>
+                        </div>
+
+                        <!-- form -->
+                        <div class="w-full border-gray-200 bg-opacity-25 p-6 border-t border-gray-200">
+                            <form>
+                                <div class="flex rounded-md overflow-hidden border border-gray-300">
+                                    <input type="text" class="flex-1 px-4 py-2 text-sm focus:outline-none">
+                                    <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+    }
+</script>
+
+<style>
+    .message-from-me{
+        --tw-bg-opacity: 0.25;
+        background: rgba(165, 180, 252, var(--tw-bg-opacity));
+    }
+
+    .message-to-me{
+        --tw-bg-opacity: 0.25;
+        background-color: rgba(229, 231, 235, var(--tw-bg-opacity))
+    }
+</style>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Web\PageController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+
+
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+    Route::get('/chat', [PageController::class, 'chat'])->name('chat');
+});
